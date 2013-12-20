2925bb93d4c7d06663e8f4d263b271f53ab3ffe7
oldlam <oldlam1117@hotmail.com>
2013-12-20 15:06:49 +0800

    Enhancement of BookingPicker
    Add methods:
    setDate()
    getDate()
    setNumOfReservation()
    getNumOfReservation()

--- src/com/example/test/BookingPicker.java
+++ src/com/example/test/BookingPicker.java
@@ -1,6 +1,7 @@
 package com.example.test;
 
-import java.text.ParseException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -8,7 +9,6 @@ import java.util.Date;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
@@ -17,16 +17,14 @@ public class BookingPicker extends LinearLayout {
 
 	static final private int MAX_PEOPLE = 20;
 	
-	private NumberPicker mNumOfPeoplePicker;
+	private NumberPicker mNumOfReservationPicker;
 	private NumberPicker mDatePicker;
 	private NumberPicker mTimePicker;
 	
 	private ArrayList<Date> mDates;
+	private ArrayList<Date> mTimes;
 	
 	private OnValueChangeListener mOnValueChangeListener;
-	
-	private SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd hh:mm a");
-	private SimpleDateFormat psdf = new SimpleDateFormat("yyyyMMdd");
 	
 	public BookingPicker(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -36,24 +34,25 @@ public class BookingPicker extends LinearLayout {
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		inflater.inflate(R.layout.view_booking_picker, this, true);
 		
-		mNumOfPeoplePicker = (NumberPicker) getChildAt(0);
+		mNumOfReservationPicker = (NumberPicker) getChildAt(0);
 		mDatePicker = (NumberPicker) getChildAt(1);
 		mTimePicker = (NumberPicker) getChildAt(2);
 		
 		mDates = new ArrayList<Date>();
+		mTimes = new ArrayList<Date>();
 		mOnValueChangeListener = null;
 	
 		
-		String numOfPeopleStrings[] = new String[MAX_PEOPLE];
-		numOfPeopleStrings[0] = "1 person";
+		String numOfReservationStrings[] = new String[MAX_PEOPLE];
+		numOfReservationStrings[0] = "1 person";
 		for (int i=1; i<MAX_PEOPLE; i++) {
-			numOfPeopleStrings[i] = (i+1)+" people";
+			numOfReservationStrings[i] = (i+1)+" people";
 		}
-		mNumOfPeoplePicker.setMinValue(1);
-		mNumOfPeoplePicker.setMaxValue(MAX_PEOPLE);
-		mNumOfPeoplePicker.setWrapSelectorWheel(false);
-		mNumOfPeoplePicker.setDisplayedValues(numOfPeopleStrings);
-		mNumOfPeoplePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+		mNumOfReservationPicker.setMinValue(1);
+		mNumOfReservationPicker.setMaxValue(MAX_PEOPLE);
+		mNumOfReservationPicker.setWrapSelectorWheel(false);
+		mNumOfReservationPicker.setDisplayedValues(numOfReservationStrings);
+		mNumOfReservationPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
 			@Override
 			public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
 				bookingValueChanged();
@@ -61,6 +60,10 @@ public class BookingPicker extends LinearLayout {
 		});
 		
 		Calendar today = Calendar.getInstance();
+		today.set(Calendar.HOUR_OF_DAY, 0);
+		today.set(Calendar.MINUTE, 0);
+		today.set(Calendar.SECOND, 0);
+		today.set(Calendar.MILLISECOND, 0);
 		Calendar oneYearLater = Calendar.getInstance();
 		oneYearLater.add(Calendar.YEAR, 1);
 		ArrayList<String> dates = new ArrayList<String>();
@@ -98,10 +101,17 @@ public class BookingPicker extends LinearLayout {
 			}
 		});
 
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
 		
 		String timeStrings[] = new String[48];
 		for (int i=0; i<48; i++) {
-			timeStrings[i] = ((i%24)/2==0?12:(i%24)/2) + ":" + (i%2==0?"00":"30") + (i<24?" am":" pm");
+			timeStrings[i] = (cal.get(Calendar.HOUR)==0?12:cal.get(Calendar.HOUR))+":"+String.format("%02d", cal.get(Calendar.MINUTE))+(cal.get(Calendar.AM_PM)==Calendar.AM?" am":" pm");
+			mTimes.add(cal.getTime());
+			cal.add(Calendar.MINUTE, 30);
 		}
 		mTimePicker.setMaxValue(47);
 		mTimePicker.setWrapSelectorWheel(false);
@@ -112,6 +122,14 @@ public class BookingPicker extends LinearLayout {
 				bookingValueChanged();
 			}
 		});
+		mTimePicker.setOnScrollListener(new NumberPicker.OnScrollListener() {
+			@Override
+			public void onScrollStateChange(NumberPicker view, int scrollState) {
+				if (scrollState == NumberPicker.OnScrollListener.SCROLL_STATE_IDLE) {
+					validateDateTime();
+				}
+			}
+		});
 	}
 	
 	public void setOnValueChangeListener(OnValueChangeListener listener) {
@@ -120,18 +138,38 @@ public class BookingPicker extends LinearLayout {
 	
 	public void bookingValueChanged() {
 		if (mOnValueChangeListener != null) {
-			int numOfPeople = mNumOfPeoplePicker.getValue();
-			String timeStrings[] = mTimePicker.getDisplayedValues();
-			String timeString = timeStrings[mTimePicker.getValue()];
-			Date date = mDates.get(mDatePicker.getValue());
-			try{
-				date = sdf.parse("" + psdf.format(date) + " " + timeString);
-			}catch(ParseException pe){
+			int numOfReservation = mNumOfReservationPicker.getValue();
+			
+			Calendar dateCal = Calendar.getInstance();
+			Calendar timeCal = Calendar.getInstance();
+			dateCal.setTime(mDates.get(mDatePicker.getValue()));
+			timeCal.setTime(mTimes.get(mTimePicker.getValue()));
+			dateCal.set(Calendar.HOUR_OF_DAY, timeCal.get(Calendar.HOUR_OF_DAY));
+			dateCal.set(Calendar.MINUTE, timeCal.get(Calendar.MINUTE));
+			
+			mOnValueChangeListener.onValueChange(numOfReservation, dateCal.getTime());
+		}
+	}
+	
+	public void validateDateTime() {
+		Calendar dateCal = Calendar.getInstance();
+		Calendar timeCal = Calendar.getInstance();
+		dateCal.setTime(mDates.get(mDatePicker.getValue()));
+		timeCal.setTime(mTimes.get(mTimePicker.getValue()));
+		dateCal.set(Calendar.HOUR_OF_DAY, timeCal.get(Calendar.HOUR_OF_DAY));
+		dateCal.set(Calendar.MINUTE, timeCal.get(Calendar.MINUTE));
+		
+		Calendar now = Calendar.getInstance();
+		if (dateCal.before(now)) {
+			for (int i=0; i<mTimes.size(); i++) {
+				Calendar cal = Calendar.getInstance();
+				cal.setTime(mTimes.get(i));
+				if (cal.get(Calendar.HOUR_OF_DAY) > now.get(Calendar.HOUR_OF_DAY) || (cal.get(Calendar.HOUR_OF_DAY) == now.get(Calendar.HOUR_OF_DAY) && cal.get(Calendar.MINUTE) > now.get(Calendar.MINUTE))) {
+					mTimePicker.setValue(i);
+					bookingValueChanged();
+					break;
+				}
 			}
-			
-			
-			
-			mOnValueChangeListener.onValueChange(numOfPeople, date);
 		}
 	}
 	
@@ -140,7 +178,52 @@ public class BookingPicker extends LinearLayout {
 	}
 	
 	public static interface OnValueChangeListener {
-		public abstract void onValueChange(int numOfPeople, Date date);
+		public abstract void onValueChange(int numOfReservation, Date date);
+	}
+	
+	public void setDate(Date date) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		for (int i=0; i<mTimes.size(); i++) {
+			Calendar timeCal = Calendar.getInstance();
+			timeCal.setTime(mTimes.get(i));
+			if (timeCal.get(Calendar.HOUR_OF_DAY) > cal.get(Calendar.HOUR_OF_DAY) || (timeCal.get(Calendar.HOUR_OF_DAY) == cal.get(Calendar.HOUR_OF_DAY) && timeCal.get(Calendar.MINUTE) > cal.get(Calendar.MINUTE))) {
+				mTimePicker.setValue(i);
+				break;
+			}
+		}
+		for (int i=0; i<mDates.size(); i++) {
+			Calendar dateCal = Calendar.getInstance();
+			dateCal.setTime(mDates.get(i));
+			cal.set(Calendar.HOUR_OF_DAY, 0);
+		    cal.set(Calendar.MINUTE, 0);
+		    cal.set(Calendar.SECOND, 0);
+		    cal.set(Calendar.MILLISECOND, 0);
+			if (cal.equals(dateCal)) {
+				mDatePicker.setValue(i);
+				break;
+			}
+		}
+		bookingValueChanged();
+	}
+	
+	public void setNumOfReservation(int numOfReservation) {
+		mNumOfReservationPicker.setValue(numOfReservation);
+		bookingValueChanged();
+	}
+	
+	public int getNumOfReservation() {
+		return mNumOfReservationPicker.getValue();
+	}
+	
+	public Date getDate() {
+		Calendar dateCal = Calendar.getInstance();
+		Calendar timeCal = Calendar.getInstance();;
+		dateCal.setTime(mDates.get(mDatePicker.getValue()));
+		timeCal.setTime(mTimes.get(mTimePicker.getValue()));
+		dateCal.set(Calendar.HOUR_OF_DAY, timeCal.get(Calendar.HOUR_OF_DAY));
+		dateCal.set(Calendar.MINUTE, timeCal.get(Calendar.MINUTE)); 
+		return dateCal.getTime();
 	}
 
 }
